e46c5655275c4340517e71c9b3046cd941210f9d
황진규 <hwangczbiz@gmail.com>
2021-03-23 13:37:45 +0900

    input clear button 추가

--- js/sub.js
+++ js/sub.js
@@ -20,6 +20,79 @@ $(function(){
         // ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip 텍스트
         // ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 부분 Tooltip 텍스트
         // ,minDate: "-1M" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
-        // ,maxDate: "+1M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)                    
+        // ,maxDate: "+1M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+        ,onSelect: function(dateText) {
+            if( inputClearHandler.isSetClear && (this.value != undefined && this.value != "") ){
+                const btn = this.parentNode.querySelector(".clear-btn");
+                inputClearHandler.addClearBtn(btn);
+            }
+        }
     })
+
+    // input clear button 생성
+    inputClearHandler.init();
 })
+
+// input clear button
+const inputClearHandler = {
+    isSetClear: false
+    , inputList: null
+    , hasClearBtnList: null
+
+    , init: function(){
+        this.isSetClear = true;
+        this.inputList = document.querySelectorAll("input");
+        this.hasClearBtnList = ["text", "password", "number"];
+        const _this = this;
+
+        for(let i=0; i<this.inputList.length; i++){
+            const item = this.inputList[i];
+            const type = item.getAttribute("type");
+            if( this.hasClearBtnList.indexOf( type ) > -1 && !item.readOnly && !item.disabled ){
+                // datepicker 별도 처리
+                if( item.classList.contains("datepicker") ){
+                    const btn = item.parentNode.appendChild(_this.createClearTag());
+                    if(item.value != undefined && item.value != ""){
+                        _this.addClearBtn(btn);
+                    }
+                }else{
+                    // span태그로 감싸고 그 안에 .clear-btn 생성
+                    let span = document.createElement("span");
+                    span.setAttribute("class", "clear-btn-con");
+                    span.innerHTML = item.outerHTML + _this.createClearTag().outerHTML;
+                    item.parentNode.insertBefore(span, item);
+                    item.parentNode.removeChild(item);
+        
+                    const newItem = span.querySelector("input");
+                    const btn = span.querySelector(".clear-btn");
+        
+                    if(newItem.value != undefined && newItem.value != ""){
+                        _this.addClearBtn(btn);
+                    }
+        
+                    newItem.addEventListener("keyup", function(){
+                        if(newItem.value != undefined && newItem.value != ""){
+                            _this.addClearBtn(btn);
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+    , createClearTag: function(){
+        const span = document.createElement("span");
+        span.setAttribute("class", "clear-btn");
+        return span;
+    }
+
+    , addClearBtn: function(btn){
+        btn.style.display = "inline-block";
+        btn.addEventListener("click", function(){
+            const input = this.parentNode.querySelector("input");
+            input.value = "";
+            input.focus();
+            btn.style.display = "none";
+        });
+    }
+}
